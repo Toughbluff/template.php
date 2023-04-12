a50e6583f1d29e959b07359bd08928c77d297aae
Toughbluff <104803072+Toughbluff@users.noreply.github.com>
2023-04-12 17:22:48 -0600

    Create template.php

---
+++ template.php
@@ -0,0 +1,13 @@
+<!DOCTYPE  HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Logic PHP</title>
+    </head>
+    <h1></h1>
+    <body>
+        <?php
+
+        ?>
+    </body>
+</html>
